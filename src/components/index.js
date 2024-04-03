2112b1e0577ec324864eb345c1d3560430a79c83
Ebuka Moses <okoyemoses83@gmail.com>
2024-04-03 20:00:08 +0100

    pushing my job

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomButton from "./CustomButton";
+import TextInput from "./TextInput";
+import SignUp from "./SignUp";
+
+export { Navbar, Footer, CustomButton, TextInput, SignUp };
